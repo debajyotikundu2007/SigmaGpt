fc069a2959ddf858e9b9916f2400d92d9c6eb210
Debajyoti Kundu <debajyotikundu2007@gmail.com>
2026-06-28 14:15:47 +0530

    fixed otp issue

--- Backend/service/email.js
+++ Backend/service/email.js
@@ -1,10 +1,11 @@
 import nodemailer from "nodemailer";
-
+console.log("BREVO_USER =", process.env.BREVO_USER);
+console.log("NODE_ENV =", process.env.NODE_ENV);
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-
+  
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS,
@@ -13,6 +14,7 @@ const transporter = nodemailer.createTransport({
 
 export async function sendEmail(to, subject, text, html) {
   try {
+    console.log("Trying to connect to Brevo...");
     const info = await transporter.sendMail({
       from: `"SigmaGPT" <${process.env.BREVO_USER}>`,
       to,
