8e220010d41782bf34fe0342e4b547f04dfe7822
Debajyoti Kundu <debajyotikundu2007@gmail.com>
2026-06-28 12:51:05 +0530

    fixed otp issue

--- Backend/service/email.js
+++ Backend/service/email.js
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host:"smpt.gmail.com",
+  port:587,
+  secure:false,
   auth: {
     type: 'OAuth2',
     user: process.env.GOOGLE_USER,
